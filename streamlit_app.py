c75d4a00134afa668fb5e09d3cb6807791dc3c44
cmkp2675 <124547334+cmkp2675@users.noreply.github.com>
2024-01-04 12:52:46 +0530

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('Had a dinner')
+streamlit.header('Breakfast')
+streamlit.text('Coffee')
+streamlit.text('Tea')
+streamlit.text('Bread')
+streamlit.text('Snacks')
+
