13c2e1972c89cdcb7b02005fadb919530afe1e9d
cmkp2675 <124547334+cmkp2675@users.noreply.github.com>
2024-01-05 21:31:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-''' import streamlit
+import streamlit
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -32,6 +32,13 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take 
 streamlit.dataframe(fruityvice_normalized) #display o/p as a table
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)'''
+streamlit.write('The user entered ', fruit_choice)
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
