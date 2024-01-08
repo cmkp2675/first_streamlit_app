5a3309ccad486999865fe0ef899e1cde7c59c6f0
cmkp2675 <124547334+cmkp2675@users.noreply.github.com>
2024-01-08 14:40:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,6 @@ else:
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
   streamlit.dataframe(fruityvice_normalized)
-  
-  
 
 
 #streamlit.write('The user entered ', fruit_choice)
