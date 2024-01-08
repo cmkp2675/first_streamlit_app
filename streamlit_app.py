a5054b237ec8193fb0638f4ef2251aa22b24e7a7
cmkp2675 <124547334+cmkp2675@users.noreply.github.com>
2024-01-08 14:40:01 +0530

    Update streamlit_app.py]

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,31 @@ fruits_to_show = my_fruit_list.loc[fruits_selected] #To show only Avocado & Stra
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-
+#New section to display Fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
-#streamlit.text(fruityvice_response.json()) #writes data in JSON format
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response.json()) #writes data in JSON format
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it
+#take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
 
-streamlit.dataframe(fruityvice_normalized) #display o/p as a table
+#display o/p as a table
+streamlit.dataframe(fruityvice_normalized) 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit choice:
+  streamlit.error ("Please select the fruit to get information")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+  streamlit.dataframe(fruityvice_normalized)
+  
+  
+
+
+#streamlit.write('The user entered ', fruit_choice)
 
 streamlit.stop #don't run anything past here
 
