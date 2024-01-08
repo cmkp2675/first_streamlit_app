40759cab71a00a812e0370fe86b3991caaa31b8d
cmkp2675 <124547334+cmkp2675@users.noreply.github.com>
2024-01-08 15:08:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display o/p as a table
 streamlit.dataframe(fruityvice_normalized) 
 
+def get_fruityvice_data(this_fruit_choice):
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+     return fruityvice_normalized
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
