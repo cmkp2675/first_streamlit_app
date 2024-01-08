46ccf9367e13ff87c5d949b461f7dfa0e8fbb49c
cmkp2675 <124547334+cmkp2675@users.noreply.github.com>
2024-01-08 14:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows) #they are closed regardless of success or failure
 
 add_my_fruit=streamlit.text_input('What fruit would you like to add', 'jackfruit')
-streamlit.write('Thanks for adding jackfruit ', fruit_choice)
+
+streamlit.write('Thanks for adding jackfruit ', add_my_fruit)
+
+
+my_cur.execute ("insert into fruit_load_list values ('from streamlit')")
